45180e1657ed16c3b978040781b09d0d9a954a46
Daniel Arndt <danielarndt@gmail.com>
2011-02-14 13:11:29 -0400

    Removed needless use of pointers.

--- rerun.cpp
+++ rerun.cpp
@@ -10,9 +10,9 @@ using namespace std;
 int _maxProgSize;
 int _dim;
 
-vector < string* >* endOfStreamToVector(ifstream &infile, int offset)
+vector < string > endOfStreamToVector(ifstream &infile, int offset)
 {
-  vector < string* >* lines = new vector < string* >();
+  vector < string > lines;
   infile.seekg(-offset, ios_base::end);
   
   /* Throw away first line */
@@ -21,23 +21,28 @@ vector < string* >* endOfStreamToVector(ifstream &infile, int offset)
   //delete tmp;
   
   while (!infile.eof()){
-    string *tmp = new string();
-    getline(infile, *tmp);
-    if (tmp->length() > 0)
-      lines->insert(lines->end(), tmp);
-    else
-      delete tmp;
+    string tmp;
+    getline(infile, tmp);
+    if (tmp.length() > 0)
+      lines.insert(lines.end(), tmp);
   }
   return lines;
 }
 
-int createLearners()
+int createLearners(vector < string* > *fileEnd)
 {
   learner *l;
+  string line;
   long action = 1;
   l = new learner(-1, action, _maxProgSize, _dim);
   
-  instruction *i = new instruction();
+  vector < string* >::iterator fiter, fiterend;
+  for (fiter = fileEnd->begin(), fiterend = fileEnd->end(); fiter != fiterend; fiter++)
+  {
+    //line = *fiter;
+    //line.
+    //instruction *i = new instruction();
+  }
   
   /* The following are variables we need to set */
 
@@ -88,8 +93,10 @@ int main(int argc,
   int foundAt = findLastOccurrence(argv[2], "explicitEnv::test");
   cout << "Found at: " << foundAt << endl;
   
-  vector < string* >* lines = endOfStreamToVector(infile, foundAt);
-  cout << "Lines read: " << lines->size() << endl;
+  vector < string > lines = endOfStreamToVector(infile, foundAt);
+  cout << "Lines read: " << lines.size() << endl;
+  cout << "First line: " << lines[9] << endl;
+  
   
   return 0;
 }
